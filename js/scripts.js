b7faada7ea82c037626c8cb33b209a3ee14bfee6
CreativeMarkus <mark@creativemarkus.com>
2025-03-13 14:23:56 -0400

    loop  updated to use pokemonRepository.getAll()

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,61 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-  { 
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"]
-  },
-  {
-    name: "Charmander",
-    height: 8,
-    types: ["fire"]
-  },
-  {
-    name: "Squirtle",
-    height: 5,
-    types: ["water"]
-  },
-  {
-    name: "Jigglypuff",
-    height: 0.5,
-    types: ["Fairy"]
-  },
-  {
-    name: "Mewtwo",
-    height: 2.0,
-    types: ["Psychic"]
-  },
-];
-function getAll() {
-  return pokemonList; 
-}
-function add(pokemon) {
-  if (
-    typeof pokemon === "object" &&
-    "name" in pokemon &&
-    "height" in pokemon &&
-    "types" in pokemon
-  ) {
-    pokemonList.push(pokemon);
-  } else {
-    console.error("Invalid Pokémon data");
+  let pokemonList = [
+    { 
+      name: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Charmander",
+      height: 8,
+      types: ["fire"]
+    },
+    {
+      name: "Squirtle",
+      height: 5,
+      types: ["water"]
+    },
+    {
+      name: "Jigglypuff",
+      height: 0.5,
+      types: ["Fairy"]
+    },
+    {
+      name: "Mewtwo",
+      height: 2.0,
+      types: ["Psychic"]
+    },
+  ];
+
+  function getAll() {
+    return pokemonList; 
   }
-}
-return {
-  getAll: getAll,
-  add: add
-};
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon &&
+      Array.isArray(pokemon.types) 
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.error("Invalid Pokémon data"); 
+    }
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
 })(); 
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let output = `${pokemon.name} (height: ${pokemon.height})`;
+  if (pokemon.height > 6.5) {
+    output += " - Wow, that's big!";
+  }
+  document.write(output + "<br>"); 
+});
